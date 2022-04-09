fd65f16964df64fa2d5b189bcae1e1feb8a2e334
Jovana <jsmilevska@yahoo.com>
2022-04-10 00:51:39 +0200

    changes

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     // baseURL: process.env.REACT_APP_HOST_ENV,
-    baseURL: `https://backendemt.herokuapp.com`,
+    baseURL: `https://backendemt.herokuapp.com/`,
     headers: {
         'Access-Control-Allow-Origin': '*'
     }
