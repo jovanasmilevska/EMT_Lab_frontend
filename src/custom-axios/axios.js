f088f88010e8a5eb63c8f50fd00a520bafe7350d
Jovana <jsmilevska@yahoo.com>
2022-04-10 00:42:29 +0200

    changed url to backend

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     // baseURL: process.env.REACT_APP_HOST_ENV,
-    baseURL: `http://localhost:8080/`,
+    baseURL: `https://backendemt.herokuapp.com`,
     headers: {
         'Access-Control-Allow-Origin': '*'
     }
